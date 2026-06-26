a23290d1e0462fe53ec10c87b868c88f2dacf626
Sobuj <sobujmadbor660@gmail.com>
2026-06-26 12:58:25 +0600

    varifytoken added

--- index.js
+++ index.js
@@ -24,26 +24,38 @@ const client = new MongoClient(uri, {
 });
  
 
-// const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
 
-// const verifyToken = async (req, res, next) => {
-//   const authHeader = req?.headers.authorization;
-//   if (!authHeader) {
-//     return res.status(401).json({ message: "Unauthorized" });
-//   }
-//   const token = authHeader.split(" ")[1];
-//   if (!token) {
-//     return res.status(401).json({ message: "Unauthorized" }); 
-//   }
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+ 
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" }); 
+  }
 
-//   try {
-//     const { payload } = await jwtVerify(token, JWKS);
-//     console.log(payload);
-//     next();
-//   } catch (error) {
-//     return res.status(403).json({ message: "Forbidden" }); 
-//   }
-// };
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    req.user = payload;
+
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" }); 
+  }
+};
+
+const librarianVerify = async (req, res, next) => {
+  const user = req.user;  
+  if (user.role !== "librarian" || user.plan != "free") {
+    return res.status(403).json({ msg: "Forbidden" });
+  }
+  next();
+};
+
 
 
 
@@ -101,7 +113,7 @@ app.patch("/user/:id", async (req, res) => {
 
 
 //librarian
- app.post("/bookpost", async (req, res) => {
+ app.post("/bookpost", verifyToken, librarianVerify, async (req, res) => {
       const requestData = req.body;
       // console.log(requestData);
       const result = await bookpostCollection.insertOne(requestData);      
@@ -247,6 +259,15 @@ app.patch("/delivery-request/:id", async (req, res) => {
 
   res.send(result);
 });
+
+
+
+
+
+
+
+
+
 
 
 
